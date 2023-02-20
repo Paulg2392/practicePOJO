3d1b93a374bc58638f269a2731746aa6a206aed0
Paulg2392 <pgonzalez2392@gmail.com>
2023-02-20 18:24:28 -0500

    update

--- src/com/coderscampus/CreateUserPOJO.java
+++ src/com/coderscampus/CreateUserPOJO.java
@@ -2,9 +2,43 @@ package com.coderscampus;
 
 public class CreateUserPOJO {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	private String username;
+	private String password;
+	private String firstName;
+	private String lastName;
+	private int unsuccesfulLoginAttempts;
+	
+	public String getUsername() {
+		return username;
 	}
-
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public int getUnsuccesfulLoginAttempts() {
+		return unsuccesfulLoginAttempts;
+	}
+	public void setUnsuccesfulLoginAttempts(int unsuccesfulLoginAttempts) {
+		this.unsuccesfulLoginAttempts = unsuccesfulLoginAttempts;
+	}
+	
+	
+	
 }
